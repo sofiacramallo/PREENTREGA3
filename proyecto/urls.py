35fd441101749404bc6ca3cc7ac254f4bd7ad393
Sofia Candela Ramallo <sofiacramallo@gmail.com>
2024-06-09 11:17:42 -0300

    borre view y cree appinicio

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from proyecto.views import inicio
+from inicio.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(" ", inicio),
+    path('', inicio),
 ]
