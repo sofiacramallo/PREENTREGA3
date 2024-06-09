5e4bfa3563a5386fb1daa7dadc407f040c2b739f
Sofia Candela Ramallo <sofiacramallo@gmail.com>
2024-06-09 10:49:55 -0300

    agregue views y urls

---
+++ proyecto/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+#from AppCoder.models import Curso
+from django.http import HttpResponse
+# Create your views here.
+def inicio(request):
+    return HttpResponse("vista inicio")
+#def curso(self):
+#    curso= Curso(nombre="Desarrollo Web", camada= "19881")
+#    curso.save()
+#    documentoDeTexto= f"Curso: {curso.nombre} Camada: {curso.camada}"
+ #  return HttpResponse(documentoDeTexto)
+#def profesores(request):
+ #   return HttpResponse("vista profesores")
+#def estudiantes(request):
+ #   return HttpResponse("vista estudiantes")
+#def entregables(request):
+ #   return HttpResponse("vista entregables")
