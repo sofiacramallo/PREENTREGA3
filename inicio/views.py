40253e7977b1aeb4bfd8dde0c68f9599aa88850e
Sofia Candela Ramallo <sofiacramallo@gmail.com>
2024-06-09 19:12:58 -0300

    guardo modificaciones template nuevo

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 #from AppCoder.models import Curso
 from django.http import HttpResponse
+from datetime import datetime
+from django.template import Template, Context
 # Create your views here.
 def inicio(request):
     return HttpResponse("vista inicio")
+def template1(request, nombre, apellido):
+    fecha = datetime.now()
+    return HttpResponse(f"<h1>Mi template </h1>-- Fecha: {fecha} -- Buenas {nombre} {apellido}")
+def template2(request, nombre, apellido):
+    archivo_abierto = open(r'C:\Users\ramal\Desktop\ProyectoFinal\PreentregaTres\template\template2.html')
+    template= Template(archivo_abierto.read())
+    archivo_abierto.close()
+    contexto = Context()
+    template_renderizado= template.render(contexto)
+
+    return HttpResponse(template_renderizado)
+
 #def curso(self):
 #    curso= Curso(nombre="Desarrollo Web", camada= "19881")
 #    curso.save()
